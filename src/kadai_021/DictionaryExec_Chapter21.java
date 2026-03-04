37fb7fc8423965351ca6cfc01faf4b79e707a641
kh025 <komachi787@gmail.com>
2026-03-04 14:10:32 +0900

    kadai_021を修正

--- src/kadai_021/DictionaryExec_Chapter21.java
+++ src/kadai_021/DictionaryExec_Chapter21.java
@@ -1,15 +1,15 @@
 package kadai_021;
 
-public class DictinaryExec_Chapter21 {
+public class DictionaryExec_Chapter21 {
 	public static void main(String[] args) {
 		
 		// インスタンス化
 		Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();
 		
 		// 検索ワードを配列にセット
-		String[] serchWords = {"apple", "banana", "grape", "prange"};
+		String[] searchWords = {"apple", "banana", "grape", "orange"};
 		
 		// 実行
-		dictionary.serch(serchWords);
+		dictionary.search(searchWords);
 	}
 }
--- src/kadai_021/Dictionary_Chapter21.java
+++ src/kadai_021/Dictionary_Chapter21.java
@@ -16,19 +16,19 @@ public class Dictionary_Chapter21 {
 			dictionary.put("lemon", "レモン");
 			dictionary.put("pear", "梨");
 			dictionary.put("kiwi", "キウィ");
-			dictionary.put("strawbetty", "いちご");
+			dictionary.put("strawberry", "いちご");
 			dictionary.put("grape", "ぶどう");
 			dictionary.put("muscat", "マスカット");
 			dictionary.put("cherry", "さくらんぼ");
 		}
 		
 	// 検索用メソッド
-	public void serch(String[] serchWords) {
-		for(String serchWord : serchWords) {
-			if(dictionary.containsKey(serchWord)) {
-				System.out.println(serchWord + "の意味は" + dictionary.get(serchWord));
+	public void search(String[] searchWords) {
+		for(String searchWord : searchWords) {
+			if(dictionary.containsKey(searchWord)) {
+				System.out.println(searchWord + "の意味は" + dictionary.get(searchWord));
 			} else {
-				System.out.println(serchWord + "は辞書に存在しません");
+				System.out.println(searchWord + "は辞書に存在しません");
 			}
 		}
 	}
